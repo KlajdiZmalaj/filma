d13e87bdaedccc8bbda4dc9008fa6596446d0c1c
Klajdi <klajdizmalaj@icloud.com>
2021-02-18 11:58:40 +0100

    Updates

---
+++ precache-manifest.f1cafc66ff442a76d56ba60f1ec5e54a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc73f6b0037c9d67f40c1a53a3839a66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9a7a062517415e4165a",
+    "url": "/static/css/2.d54708ad.chunk.css"
+  },
+  {
+    "revision": "0ffcf166ee71098e898f",
+    "url": "/static/css/main.b92d73e0.chunk.css"
+  },
+  {
+    "revision": "b9a7a062517415e4165a",
+    "url": "/static/js/2.dd24fd5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.dd24fd5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ffcf166ee71098e898f",
+    "url": "/static/js/main.e0fd5cac.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
