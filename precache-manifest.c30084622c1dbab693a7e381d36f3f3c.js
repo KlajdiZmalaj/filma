a0cc4223f22679da2ab4af78646087be85472a92
Klajdi <klajdizmalaj@icloud.com>
2021-02-19 17:53:41 +0100

    Updates

---
+++ precache-manifest.c30084622c1dbab693a7e381d36f3f3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3215b6b075166e4d4ed61ca7f0286c67",
+    "url": "/filma/build/index.html"
+  },
+  {
+    "revision": "b9a7a062517415e4165a",
+    "url": "/filma/build/static/css/2.d54708ad.chunk.css"
+  },
+  {
+    "revision": "0efe01b9c7907aaae5a4",
+    "url": "/filma/build/static/css/main.b92d73e0.chunk.css"
+  },
+  {
+    "revision": "b9a7a062517415e4165a",
+    "url": "/filma/build/static/js/2.dd24fd5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/filma/build/static/js/2.dd24fd5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0efe01b9c7907aaae5a4",
+    "url": "/filma/build/static/js/main.7f264bfa.chunk.js"
+  },
+  {
+    "revision": "9b825c82bc10f87c4b15",
+    "url": "/filma/build/static/js/runtime-main.79ae4a0b.js"
+  }
+]);
